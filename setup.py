9a292170c5a06e445cf739c4757a7ec5706479fa
grimnar <rechaussatjy@gmail.com>
2019-12-29 15:15:36 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pynohtml-grimnar", # Replace with your own username
+    version="0.0.1",
+    author="grimnar",
+    author_email="grimnar@gmail.com",
+    description="framework to generate html/css/js",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/grimnar/pynohtml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
