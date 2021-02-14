c92b835592348a9f0d982295e05cd591da0d965c
root <root@grim-PC.localdomain>
2021-02-14 12:21:10 +0100

    clean pynohtml.py

--- pynohtml/pynohtml.py
+++ pynohtml/pynohtml.py
@@ -1,18 +1,11 @@
 import cherrypy
 from containers import (
     HtmlMaker,
-    List,
-    Table,
-    ClassicIcon,
-    Accordion,
-    Paragraph
 )
 from fundamentals import (
-    Container,
     Element,
     ImportsLibrary
 )
-
 
 
 def pynohtml(func):
@@ -31,32 +24,3 @@ def pynohtml(func):
         html = HtmlMaker("hello world", result)
         return html.html
     return wrapper
-
-
-class Root(object):
-    @pynohtml
-    def index(self):
-        li = List([1, 2, 3, 4])
-        icon = ClassicIcon("cloud",
-                           font="google",
-                           klass="material-icons",
-                           label="not_interested",
-                           )
-        tab = Table([[1, 2, 3], [1, 2, 3], [1, 2, icon]],
-                    header=["A", "B", "C"],
-                    tfoout=["a", "b", "c"])
-        acc = Accordion({"Section 1": Paragraph("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
-                        "Section 2": Paragraph("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
-                        "Section 3": Paragraph("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."),
-                        })
-        mainContainer = Container([li, tab, acc])
-        return mainContainer.html
-
-    @pynohtml
-    def other(self):
-        l = List([1, 2, 3, 4])
-        return l
-
-
-if __name__ == '__main__':
-    cherrypy.quickstart(Root(), '/')
